2e1167586b80bab34e4f0a012524cfe49e8aacc1
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2022-05-31 00:51:06 -0700

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
+variable "pm_api_url" {
+  default     = ""
+  description = "URL for Proxmox API"
+}
+
+variable "pm_api_token_id" {
+  default     = ""
+  description = "Token ID for Proxmox API"
+}
+
+variable "pm_api_token_secret" {
+  default     = ""
+  description = "Token for Proxmox API"
+}
+
 variable "target_node" {
-  default = "host-1"
+  default     = "host-1"
   description = "Proxmox cluster node to target for deployment"
 }
