97d22e1121effc372d08e182c7689459e71d2ce2
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2022-10-05 01:46:59 -0700

    Updated Zandronum

--- zandronum.tf
+++ zandronum.tf
@@ -44,7 +44,7 @@ resource "proxmox_lxc" "zandronum" {
     type        = "ssh"
     user        = "root"
     private_key = file(var.private_ssh_key)
-    host        = split("/", one(proxmox_lxc.zandronum[*].network[0].ip))[0]
+    host        = split("/", self.network[0].ip)[0]
   }
 
   provisioner "remote-exec" {
@@ -54,12 +54,12 @@ resource "proxmox_lxc" "zandronum" {
   provisioner "local-exec" {
     command = "${path.module}/scripts/ansible_deploy.sh"
     environment = {
-      LIVEDNS_API_KEY = var.livedns_api_key
-      LIVEDNS_DOMAIN = var.livedns_domain
-      LIVEDNS_SUBDOMAIN = "zandronum"
+      LINODEDNS_DOMAIN = var.linodedns_domain
+      LINODEDNS_SUBDOMAIN = "zandronum"
+      LINODEDNS_TOKEN = var.linodedns_token
       PLAYBOOK = "zandronum.yml"
       PRIVATE_SSH_KEY = var.private_ssh_key
-      TARGET = split("/", one(proxmox_lxc.zandronum[*].network[0].ip))[0]
+      TARGET = split("/", self.network[0].ip)[0]
       ZANDRONUM_RCON_PASS = var.zandronum_rcon_pass
       ZANDRONUM_SERVER_PASS = var.zandronum_server_pass
     }
