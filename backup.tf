c27c1a9f5edd07deb6cffd2616add37afa99eb54
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2022-12-12 22:36:28 -0800

    Added backup role

---
+++ backup.tf
@@ -0,0 +1,67 @@
+resource "random_password" "backup_password" {
+  length  = 16
+  special = true
+}
+
+resource "proxmox_lxc" "backup" {
+  count           = 0
+  target_node     = var.target_node
+  hostname        = "backup-${count.index}"
+  onboot          = true
+  ostemplate      = "images:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  password        = random_password.backup_password.result
+  ssh_public_keys = join("\n", [file(var.public_ssh_key), file(var.public_backup_ssh_key)])
+  start           = true
+  unprivileged    = true
+
+  cores           = 2
+  memory          = 2048
+  swap            = 1024
+
+  rootfs {
+    storage = "images"
+    size    = "20G"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    ip       = "192.168.2.38/24"
+    ip6      = "dhcp"
+    gw       = "192.168.2.1"
+    firewall = true
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file(var.private_ssh_key)
+    host        = self.network[0].ip
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y"]
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/ansible_deploy.sh"
+    environment = {
+      PLAYBOOK = "backup.yml"
+      PRIVATE_SSH_KEY = var.private_ssh_key
+      ANSIBLE_RCLONE_CONFIGS = var.ansible_rclone_configs
+      RCLONE_BACKUP_PRIVATESSHKEY = var.private_backup_ssh_key
+      RCLONE_BACKUP_SOURCES = var.rclone_backup_sources
+      RCLONE_BACKUP_TARGET = var.rclone_backup_target
+      TARGET = self.network[0].ip
+    }
+  }
+}
+
+output "backup_ip" {
+  value = one(proxmox_lxc.backup[*].network[0].ip)
+}
+
+output "backup_password" {
+  value     = random_password.backup_password.result
+  sensitive = true
+}
--- variables.tf
+++ variables.tf
@@ -1,36 +1,49 @@
+variable "ansible_rclone_configs" {
+  default     = ""
+  type        = string
+  description = "Config YAML for ansible_rclone"
+}
+
 variable "ansible_repo" {
   default     = "https://github.com/frozenfoxx/ansible-bricksandblocks.git"
+  type        = string
   description = "Repository containing Ansible playbooks"
 }
 
 variable "barotrauma_clientperm" {
   default     = ""
+  type        = string
   description = "Space delimited, colon interior-delimited list of users with Ranks for Barotrauma (ex. \"Frozen:[steam64]:Admin\")"
 }
 
 variable "barotrauma_server_msg" {
   default     = "Welcome to the Church of Foxx Barotrauma server, let Frozen know if you experience issues."
+  type        = string
   description = "Message of the day for a Barotrauma server"
 }
 
 variable "barotrauma_server_name" {
   default     = "Church of Foxx"
+  type        = string
   description = "Name of a Barotrauma server"
 }
 
 variable "barotrauma_server_pass" {
   default     = ""
+  type        = string
   description = "Password for connecting to a Barotrauma server"
   sensitive   = true
 }
 
 variable "linodedns_domain" {
   default     = "bricksandblocks.net"
+  type        = string
   description = "Domain used for Linode DNS"
 }
 
 variable "linodedns_subdomain" {
   default     = "*"
+  type        = string
   description = "Subdomain used for Linode DNS"
 }
 
@@ -42,71 +55,96 @@ variable "linodedns_token" {
 
 variable "livedns_api_key" {
   default     = ""
+  type        = string
   description = "API key for usage with LiveDNS"
   sensitive   = true
 }
 
 variable "livedns_domain" {
   default     = "bricksandblocks.net"
+  type        = string
   description = "Domain used for LiveDNS"
 }
 
 variable "livedns_subdomain" {
   default     = "*"
+  type        = string
   description = "Subdomain used for LiveDNS"
 }
 
 variable "pm_api_url" {
   default     = ""
+  type        = string
   description = "URL for Proxmox API"
 }
 
 variable "pm_user" {
   default     = ""
+  type        = string
   description = "User ID for Proxmox API"
 }
 
 variable "pm_pass" {
   default     = ""
+  type        = string
   description = "Password for Proxmox API"
   sensitive   = true
 }
 
 variable "private_ssh_key" {
   default     = "~/.ssh/id_rsa"
+  type        = string
   description = "SSH Private Key"
   sensitive   = true
 }
 
 variable "private_backup_ssh_key" {
   default     = "~/.ssh/backup_id_rsa"
+  type        = string
   description = "SSH Private Key"
   sensitive   = true
 }
 
 variable "public_ssh_key" {
   default     = "~/.ssh/id_rsa.pub"
+  type        = string
   description = "SSH Public Key Fingerprint"
 }
 
 variable "public_backup_ssh_key" {
   default     = "~/.ssh/backup_id_rsa.pub"
+  type        = string
   description = "SSH Public Key Fingerprint"
+}
+
+variable "rclone_backup_sources" {
+  default     = ""
+  type        = string
+  description = "Colon-separated, space-delimited list of backup sources and types to run (ex. \"someserver-2:barotrauma otherserver-5:library\")"
+}
+
+variable "rclone_backup_target" {
+  default     = ""
+  type        = string
+  description = "Target host for backups"
 }
 
 variable "target_node" {
   default     = "host-1"
+  type        = string
   description = "Proxmox cluster node to target for deployment"
 }
 
 variable "zandronum_rcon_pass" {
   default     = ""
+  type        = string
   description = "Password for connecting to a Zandronum server with RCON"
   sensitive   = true
 }
 
 variable "zandronum_server_pass" {
   default     = ""
+  type        = string
   description = "Password for connecting to a Zandronum server"
   sensitive   = true
 }
