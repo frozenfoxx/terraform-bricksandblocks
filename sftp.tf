66f90cd8070263a817e7188700a4be05232312a8
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2023-04-30 09:47:54 -0700

    Added sftp server

---
+++ sftp.tf
@@ -0,0 +1,67 @@
+resource "random_password" "sftp_password" {
+  length  = 16
+  special = true
+}
+
+resource "proxmox_lxc" "sftp" {
+  count           = 1
+  target_node     = var.target_node
+  hostname        = "sftp"
+  onboot          = true
+  ostemplate      = "images:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  password        = random_password.sftp_password.result
+  ssh_public_keys = file(var.public_ssh_key)
+  start           = true
+  unprivileged    = true
+
+  cores           = 2
+  memory          = 2048
+
+  rootfs {
+    storage = "pool"
+    size    = "10G"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    ip       = "192.168.2.17/24"
+    ip6      = "dhcp"
+    gw       = "192.168.2.1"
+    firewall = true
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file(var.private_ssh_key)
+    host        = split("/", self.network[0].ip)[0]
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y"]
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/ansible_deploy.sh"
+    environment = {
+      ANSIBLE_REPO = var.ansible_repo
+      INVENTORY_PATH = var.ansible_inventory_path
+      RCLONE_CONFIG_INVENTORY_ACCOUNT = var.ansible_rclone_config_inventory_account
+      RCLONE_CONFIG_INVENTORY_KEY = var.ansible_rclone_config_inventory_key
+      RCLONE_CONFIG_INVENTORY_TYPE = var.ansible_rclone_config_inventory_type
+      PLAYBOOK = "sftp.yml"
+      PRIVATE_SSH_KEY = var.private_ssh_key
+      TARGET = split("/", self.network[0].ip)[0]
+    }
+  }
+}
+
+output "sftp_ip" {
+  value = one(proxmox_lxc.sftp[*].network[0].ip)
+}
+
+output "sftp_password" {
+  value     = random_password.sftp_password.result
+  sensitive = true
+}
