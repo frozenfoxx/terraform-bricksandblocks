c4d14ba7e0ad9e365b2fe10cc72d17c4ac100ce1
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2023-07-22 19:38:43 -0700

    Updated mealie config

--- mealie.tf
+++ mealie.tf
@@ -15,11 +15,13 @@ resource "proxmox_vm_qemu" "mealie" {
 
   cores        = 2
   memory       = 4096
+  bios         = "ovmf"
   ipconfig0    = "ip=192.168.2.23/24,gw=192.168.2.1"
   sshkeys      = join("", [for key in var.public_ssh_keys : file(key)])
 
   disk {
     type     = "scsi"
+    scsihw   = "virtio-scsi-pci"
     #FIXME Required due to this error: https://github.com/Telmate/terraform-provider-proxmox/issues/460
     iothread = 0
     storage  = "pool"
