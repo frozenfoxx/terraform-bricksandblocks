7b42b1c2ed8d8ce6199d20087d86855842e75137
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2023-08-04 19:38:16 -0700

    Updated to cluster-1

--- cluster-1.tf
+++ cluster-1.tf
@@ -1,23 +1,23 @@
-resource "random_password" "mealie_password" {
+resource "random_password" "cluster-1_password" {
   length  = 16
   special = true
 }
 
-resource "proxmox_vm_qemu" "mealie" {
+resource "proxmox_vm_qemu" "cluster-1" {
   os_type      = "cloud-init"
   count        = 1
   clone        = var.template
-  name         = "mealie"
+  name         = "cluster-1"
   target_node  = var.target_node
   onboot       = true
   agent        = 0
   qemu_os      = "other"
 
-  cores        = 2
+  cores        = 4
   memory       = 4096
   bios         = "seabios"
   scsihw       = "virtio-scsi-pci"
-  ipconfig0    = "ip=192.168.2.23/24,gw=192.168.2.1"
+  ipconfig0    = "ip=192.168.2.25/24,gw=192.168.2.1"
   sshkeys      = join("", [for key in var.public_ssh_keys : file(key)])
 
   disk {
@@ -25,7 +25,7 @@ resource "proxmox_vm_qemu" "mealie" {
     #FIXME Required due to this error: https://github.com/Telmate/terraform-provider-proxmox/issues/460
     iothread = 0
     storage  = "pool"
-    size     = "10G"
+    size     = "20G"
   }
 
   network {
@@ -37,7 +37,7 @@ resource "proxmox_vm_qemu" "mealie" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file(var.private_ssh_key)
-    host        = "192.168.2.23"
+    host        = "192.168.2.25"
   }
 
   provisioner "remote-exec" {
@@ -47,24 +47,24 @@ resource "proxmox_vm_qemu" "mealie" {
   provisioner "local-exec" {
     command = "${path.module}/scripts/ansible_deploy.sh"
     environment = {
-      ANSIBLE_DIR = "ansible-mealie"
+      ANSIBLE_DIR = "ansible-cluster-1"
       ANSIBLE_REPO = var.ansible_repo
       INVENTORY_PATH = var.ansible_inventory_path
       RCLONE_CONFIG_INVENTORY_ACCOUNT = var.ansible_rclone_config_inventory_account
       RCLONE_CONFIG_INVENTORY_KEY = var.ansible_rclone_config_inventory_key
       RCLONE_CONFIG_INVENTORY_TYPE = var.ansible_rclone_config_inventory_type
-      PLAYBOOK = "mealie.yml"
+      PLAYBOOK = "cluster-scheduler.yml"
       PRIVATE_SSH_KEY = var.private_ssh_key
-      TARGET = "192.168.2.23"
+      TARGET = "192.168.2.25"
     }
   }
 }
 
-output "mealie_ip" {
-  value = "192.168.2.23"
+output "cluster-1_ip" {
+  value = "192.168.2.25"
 }
 
-output "mealie_password" {
-  value     = random_password.mealie_password.result
+output "cluster-1_password" {
+  value     = random_password.cluster-1_password.result
   sensitive = true
 }
