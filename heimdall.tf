7a3671044192dcbaac2016477723d33b6a41a477
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2023-10-05 14:52:29 -0700

    Added heimdall

---
+++ heimdall.tf
@@ -0,0 +1,71 @@
+resource "random_password" "heimdall_password" {
+  length  = 16
+  special = true
+}
+
+resource "proxmox_vm_qemu" "heimdall" {
+  os_type      = "cloud-init"
+  count        = 1
+  clone        = var.template
+  name         = "heimdall"
+  target_node  = var.target_node
+  onboot       = true
+  agent        = 0
+  qemu_os      = "other"
+
+  cores        = 2
+  memory       = 4096
+  bios         = "seabios"
+  scsihw       = "virtio-scsi-pci"
+  ipconfig0    = "ip=192.168.2.26/24,gw=192.168.2.1"
+  sshkeys      = join("", [for key in var.public_ssh_keys : file(key)])
+
+  disk {
+    type     = "scsi"
+    #FIXME Required due to this error: https://github.com/Telmate/terraform-provider-proxmox/issues/460
+    iothread = 0
+    storage  = "pool"
+    size     = "10G"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(var.private_ssh_key)
+    host        = "192.168.2.26"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y"]
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/ansible_deploy.sh"
+    environment = {
+      ANSIBLE_DIR = "ansible-heimdall"
+      ANSIBLE_REPO = var.ansible_repo
+      INVENTORY_PATH = var.ansible_inventory_path
+      RCLONE_CONFIG_INVENTORY_ACCOUNT = var.ansible_rclone_config_inventory_account
+      RCLONE_CONFIG_INVENTORY_KEY = var.ansible_rclone_config_inventory_key
+      RCLONE_CONFIG_INVENTORY_TYPE = var.ansible_rclone_config_inventory_type
+      PLAYBOOK = "heimdall.yml"
+      PRIVATE_SSH_KEY = var.private_ssh_key
+      SSH_USER = "ubuntu"
+      TARGET = "192.168.2.26"
+    }
+  }
+}
+
+output "heimdall_ip" {
+  value = "192.168.2.26"
+}
+
+output "heimdall_password" {
+  value     = random_password.heimdall_password.result
+  sensitive = true
+}
