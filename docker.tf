3aa49c5b888baa667c0390ba948f36e01959672a
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2025-01-29 23:30:30 -0800

    Updated docker

--- docker.tf
+++ docker.tf
@@ -1,6 +1,6 @@
 resource "proxmox_vm_qemu" "docker-host-1" {
   os_type     = "cloud-init"
-  count       = 3
+  count       = 2
   clone       = var.template
   name        = "docker-${count.index + 1}"
   target_node = "host-1"
@@ -57,7 +57,7 @@ resource "proxmox_vm_qemu" "docker-host-2" {
   os_type     = "cloud-init"
   count       = 2
   clone       = var.template
-  name        = "docker-${count.index + 4}"
+  name        = "docker-${count.index + 3}"
   target_node = "host-2"
   onboot      = true
   agent       = 0
@@ -68,7 +68,7 @@ resource "proxmox_vm_qemu" "docker-host-2" {
   memory    = 8192
   bios      = "seabios"
   scsihw    = "virtio-scsi-pci"
-  ipconfig0 = "ip=192.168.2.${count.index + 27}/24,gw=192.168.2.1"
+  ipconfig0 = "ip=192.168.2.${count.index + 26}/24,gw=192.168.2.1"
   sshkeys   = join("", [for key in var.public_ssh_keys : file(key)])
 
   disks {
@@ -100,62 +100,7 @@ resource "proxmox_vm_qemu" "docker-host-2" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file(var.private_ssh_key)
-    host        = "192.168.2.${count.index + 27}"
-  }
-
-  provisioner "remote-exec" {
-    inline = ["sudo apt update", "sudo apt install python3 -y"]
-  }
-}
-
-resource "proxmox_vm_qemu" "docker-host-3" {
-  os_type     = "cloud-init"
-  count       = 2
-  clone       = var.template
-  name        = "docker-${count.index + 6}"
-  target_node = "host-3"
-  onboot      = true
-  agent       = 0
-  qemu_os     = "other"
-  tags        = "docker"
-
-  cores     = 4
-  memory    = 8192
-  bios      = "seabios"
-  scsihw    = "virtio-scsi-pci"
-  ipconfig0 = "ip=192.168.2.${count.index + 29}/24,gw=192.168.2.1"
-  sshkeys   = join("", [for key in var.public_ssh_keys : file(key)])
-
-  disks {
-    ide {
-      ide2 {
-        cloudinit {
-          storage = "images"
-        }
-      }
-    }
-    scsi {
-      scsi0 {
-        disk {
-          asyncio  = "threads"
-          iothread = true
-          storage  = "images"
-          size     = "100G"
-        }
-      }
-    }
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file(var.private_ssh_key)
-    host        = "192.168.2.${count.index + 29}"
+    host        = "192.168.2.${count.index + 26}"
   }
 
   provisioner "remote-exec" {
